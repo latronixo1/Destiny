94ef56efca17562670ea8bfbfa58a67194aa9cd5
latronixo <pamerra@mail.ru>
2025-02-07 11:48:08 +0300

    Прорешал 112 видео

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,14 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    
-    
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = storyBrain.stories[1].title
+        choice1Button.setTitle(storyBrain.stories[1].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[1].choice2, for: .normal)
+        
     }
 
-
+    @IBAction func choiseMade(_ sender: UIButton) {
+    }
+    
+    func updateUI()
 }
 
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,11 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    //массив с историями
+    let stories = [
+        Story(title: "Вы видите развилку на дороге", choice1: "Поверну налево", choice2: "Поверну направо"),
+        Story(title: "Вы видите тигра", choice1: "Позову на помощь", choice2: "Притворюсь мёртвым"),
+        Story(title: "Вы видите развилку на дороге", choice1: "Поверну налево", choice2: "Поверну направо"),
+    ]
+}
