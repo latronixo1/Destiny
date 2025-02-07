5942c878755209e9f46aa92c21cdcb54896bafff
latronixo <pamerra@mail.ru>
2025-02-07 10:48:25 +0300

    Valentin's initial commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
